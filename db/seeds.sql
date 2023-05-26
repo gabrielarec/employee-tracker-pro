007897aa9e1855f8a0f55c464b289e17eebe6bba
Gabriela <gabriela.reyna89@icloud.com>
2023-05-26 13:54:55 -0400

    fixing connection errors to run seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,23 +2,28 @@ INSERT INTO department (name)
 VALUES ("Engineering"),
 ("Finance"),
 ("Legal"),
-("Sales",);
+("Sales");
+
+SELECT * FROM department;
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("quality control", 150000, 1),
-       ("Account Manager", 20000, 2),
-       ("Associate Attorney",350000, 3),
-       ("Chief sales", 300000,4),
-       ("Sales Support", 95000,4);
+("Account Manager", 20000, 2),
+("Associate Attorney", 350000, 3),
+("Chief sales", 300000, 4),
+("Sales Support", 95000, 4);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id);
-VALUES  ("Zeialh","Reyna", 3, 1),
-        ("Faidy","Aguilar", 4, 2),
-        ("Abby","Branden", 4, 2),
-        ("Morgan","Davenport", 3, 1),
-        ("Matthew","Pollak", 1, 3 ),
-        ("Lionel","Messi", 4, 4 ),
-        ("Mary","lozano", 2, 3 ),
-        ("Vince","Vaughn", 1, 4 ),
-        ("Justin","Bieber", 4, 4 )
-        ;
+SELECT * FROM role;
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Zeialh", "Reyna", 3, 1),
+("Faidy", "Aguilar", 4, 2),
+("Abby", "Brandenburg", 4, 2),
+("Morgan", "Davenport", 3, 1),
+("Matthew", "Pollak", 1, 3),
+("Lionel", "Messi", 4, 4),
+("Mary", "lozano", 2, 3),
+("Vince", "Vaughn", 1, 4),
+("Justin", "Bieber", 4, 4);
+
+SELECT * FROM employees;
--- server.js
+++ server.js
@@ -2,22 +2,68 @@ const express = require('express');
 // import and require mysql2
 const mysql = require('mysql2');
 
-application.use(express.urlencoded({extended:false}));
-application.use(express.json());
+const app = express ();
+const PORT = process.env.PORT || 3003;
 
-// Express 
+
+//express middleware
+app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
 // creating connection to database
-const connection = mysql.createConnection({
+const db = mysql.createConnection(
+  {
     host: 'localhost',
     user: 'root',
     password:'123',
     database: 'employees_db'
-  },
-  console.log('Connected to employees_db database.')
-  );
+});
 
-  app.listen(PORT, ()=> {
-    console.log(`Server running on port ${PORT}`);
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to employees_db database.');
+});
+
+app.get('/departments', (req, res) => {
+  db.query('SELECT * FROM department', (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.json(results);
+    }
   });
+});
+
+app.get('/role', (req, res) => {
+  db.query('SELECT * FROM role', (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.json(results);
+    }
+  });
+});
+
+app.get('/employees', (req, res) => {
+  db.query('SELECT * FROM employees', (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.json(results);
+    }
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
